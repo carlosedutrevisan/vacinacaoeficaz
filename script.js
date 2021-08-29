95e3bc747cc7da517c8f84264cf2777e121afed3
carlosedutrevisan <71397052+carlosedutrevisan@users.noreply.github.com>
2021-08-29 19:58:16 -0300

    Adição do mapa

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("teste");
+var map = L.map("map").setView([0, 0], 1);
+L.tileLayer(
+  "https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=hKsUwUx2YBOu2Scruilx",
+  {
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    attribution:
+      '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+    crossOrigin: true,
+  }
+).addTo(map);
